4295966732ae631497cc6b69bda29adbc74ec719
Razvan <tzumby@gmail.com>
2019-01-13 00:05:13 -0500

    Add BIP32 256bit mnemonic generator from random entropy

---
+++ README.md
@@ -0,0 +1,21 @@
+# BitcoinAddresses
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `bitcoin_addresses` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:bitcoin_addresses, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/bitcoin_addresses](https://hexdocs.pm/bitcoin_addresses).
+
---
+++ lib/block_keys/bip32_mnemonic.ex
@@ -0,0 +1,49 @@
+defmodule BlockKeys::Bip32Mnemonic do
+  def work() do
+    entropy = SecureRandom.random_bytes(32)
+
+    checksum = 
+      entropy
+      |> entropy_hash
+      |> extract_checksum
+
+    seed = entropy <> << checksum >> 
+
+    sequences(seed, [])
+    |> to_mnemonic(words())
+  end
+
+  def sequences(<< seq :: size(11), rest :: bitstring >>, output) do
+    sequences(rest, [seq] ++ output)
+  end
+  def sequences(<<>>, output), do: output
+
+  def entropy_hash(sequence) do
+    sequence 
+    |> :libsecp256k1.sha256
+  end
+
+  def extract_checksum(<< checksum :: size(8), bits :: bitstring >>) do
+    checksum
+  end
+
+  def chunk_bits(binary, n) do
+    for << chunk::size(n) <- binary >>, do: <<chunk::size(n)>>
+  end
+
+  def words do
+    "./assets/english.txt"
+    |> File.stream!
+    |> Stream.map(&String.trim/1)
+    |> Enum.to_list
+    |> List.to_tuple
+  end
+
+  def to_mnemonic(sequences, words) do
+    sequences
+    |> Enum.map(fn seq ->
+      words
+      |> Kernel.elem(seq)
+    end)
+  end
+end
