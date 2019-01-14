090b1dcfa7fa64930cd3ec699836f52feb0c1870
Razvan <tzumby@gmail.com>
2019-01-14 15:47:38 -0500

    Add word phrase to entropy implementation

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 **WIP Elixir implementation of BIP44 Blockchain Wallet**
 
+Do not use this, it's just a learning implementation.
+
 ## Installation
 
 If [available in Hex](https://hex.pm/docs/publish), the package can be installed
@@ -21,15 +23,14 @@ be found at [https://hexdocs.pm/bitcoin_addresses](https://hexdocs.pm/bitcoin_ad
 
 ## BIP32 Mnemonic
 
+### Generate 24 word phrase
+
 The mnemonic generator from random entropy is currently implemented. 
 The BIP32Mnemonic module will output 24 mnemonic words corresponding to a 256bit entropy.
 
 ```
-iex(1)> BlockKeys.Bip32Mnemonic.generate
-["ski", "theory", "kind", "crucial", "entire", "genre", "narrow", "walk",
- "road", "match", "help", "virtual", "float", "peace", "stumble", "clock",
- "hub", "elephant", "flight", "unique", "envelope", "hungry", "dog", "verify"]
-
+iex(1)> BlockKeys.Bip32Mnemonic.generate_phrase
+"baby shadow city tower diamond magnet avocado champion crash evolve circle chair boring runway remain fantasy finger impose crumble profit excuse group twist purse"
 ```
 
 The steps to generate the mnemonic words are the following:
@@ -40,3 +41,20 @@ The steps to generate the mnemonic words are the following:
 - Split result into 11 bit sequences -> 24 sequences
 - Map each 11 bit value to a word in the predefined dictionary
 
+### Restore entropy given phrase
+
+```
+iex(1)> BlockKeys.Bip32Mnemonic.entropy_from_phrase("fade joy announce clever yellow special near expand bus jealous memory usual just daughter bring oppose tone bind cloud mosquito route warfare engage champion")
+<<81, 207, 16, 37, 21, 79, 241, 161, 228, 226, 129, 30, 238, 242, 43, 248, 23,
+  150, 111, 135, 12, 220, 228, 66, 200, 175, 200, 11, 201, 238, 18, 145>>
+```
+
+The steps to restore the entropy given the phrase are:
+
+- Split the string into individual words
+- Look up the index in the word dictionary for each word and create a list
+- Convert each element in that list to a binary string of 0s and 1s
+- Remove checksum (last 8 bits)
+- Split the bitstring into groups of 8 bits
+- Convert each chunk into a byte (8 bits)
+- Convert the byte list into a binary
--- lib/block_keys/bip32_mnemonic.ex
+++ lib/block_keys/bip32_mnemonic.ex
@@ -1,35 +1,53 @@
 defmodule BlockKeys.Bip32Mnemonic do
-  def generate do
+  @pad_length_mnemonic 8
+  @pad_length_phrase 11
+
+  def generate_phrase do
     entropy = SecureRandom.random_bytes(32)
 
-    checksum = 
-      entropy
-      |> entropy_hash
-      |> extract_checksum
-
-    seed = entropy <> << checksum >> 
-
-    sequences(seed, [])
-    |> to_mnemonic(words())
+    entropy
+      |> entropy_hash()
+      |> extract_checksum()
+      |> append_checksum(entropy)
+      |> :binary.bin_to_list()
+      |> Enum.map(fn byte -> to_bitstring(byte, @pad_length_mnemonic) end)
+      |> Enum.join()
+      |> mnemonic()
   end
 
-  def sequences(<< seq :: size(11), rest :: bitstring >>, output) do
-    sequences(rest, [seq] ++ output)
-  end
-  def sequences(<<>>, output), do: output
+  # hash the initial entropy
+  defp entropy_hash(sequence), do: :libsecp256k1.sha256(sequence)
 
-  def entropy_hash(sequence) do
-    sequence 
-    |> :libsecp256k1.sha256
+  # extract the first byte (8bits)
+  defp extract_checksum(<< checksum :: size(8), _bits :: bitstring >>), do: checksum
+
+  # append the checksum to initial entropy
+  defp append_checksum(checksum, entropy), do: entropy <> << checksum >>
+
+  # convert a byte to a bitstring (8bits)
+  def to_bitstring(byte, pad_length) do
+    byte
+    |> Integer.to_string(2)
+    |> String.pad_leading(pad_length, "0")
   end
 
-  def extract_checksum(<< checksum :: size(8), bits :: bitstring >>) do
-    checksum
+  # split the 264bit string into groups of 11, convert to base 10 integer, map it to word list
+  def mnemonic(entropy) do
+    Regex.scan(~r/.{11}/, entropy)
+    |> List.flatten()
+    |> Enum.map(fn binary -> 
+      word_index(binary, words())
+    end)
+    |> Enum.join(" ")
   end
 
-  def chunk_bits(binary, n) do
-    for << chunk::size(n) <- binary >>, do: <<chunk::size(n)>>
+  def word_index(binary, words) do
+    binary
+    |> String.to_integer(2)
+    |> element_at_index(words)
   end
+
+  defp element_at_index(index, words), do: Kernel.elem(words, index)
 
   def words do
     "./assets/english.txt"
@@ -39,11 +57,38 @@ defmodule BlockKeys.Bip32Mnemonic do
     |> List.to_tuple
   end
 
-  def to_mnemonic(sequences, words) do
-    sequences
-    |> Enum.map(fn seq ->
+  # convert the phrase to entropy
+  def entropy_from_phrase(phrase) do
+    phrase
+    |> phrase_to_list
+    |> word_indexes(words())
+    |> Enum.map(fn index -> to_bitstring(index, @pad_length_phrase) end)
+    |> Enum.join()
+    |> remove_checksum
+    |> entropy()
+  end
+
+  def entropy(bitstring) do
+    Regex.scan(~r/.{8}/, bitstring)
+    |> List.flatten
+    |> Enum.map(&String.to_integer(&1, 2))
+    |> :binary.list_to_bin()
+  end
+
+  def remove_checksum(bitstring), do: String.slice(bitstring, 0..255)
+
+  def phrase_to_list(phrase) do
+    phrase
+    |> String.split()
+    |> Enum.map(&String.trim/1)
+  end
+
+  def word_indexes(phrase_list, words) do
+    phrase_list
+    |> Enum.map(fn phrase_word ->
       words
-      |> Kernel.elem(seq)
+      |> Tuple.to_list
+      |> Enum.find_index(fn el -> el === phrase_word end)
     end)
   end
 end
