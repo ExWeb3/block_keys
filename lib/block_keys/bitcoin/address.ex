41e608234bcfe21007d012b9c52b51582e51f5bd
Razvan <tzumby@gmail.com>
2019-01-23 15:28:43 -0500

    Refactor Bitcoin and Ethereum address modules

--- lib/block_keys/bitcoin/address.ex
+++ lib/block_keys/bitcoin/address.ex
@@ -1,16 +1,18 @@
 defmodule BlockKeys.Bitcoin.Address do
   alias BlockKeys.Bip32Mnemonic
 
-  def from_public_key(<< "xpub", _rest::binary >> = encoded_key) do
-    decoded_key = encoded_key
-                  |> Bip32Mnemonic.parse_extended_key
-
-    from_public_key(decoded_key.key)
-  end
-
   def from_public_key(public_key) do
     public_key
+    |> maybe_decode()
     |> Bip32Mnemonic.hash160
     |> Bip32Mnemonic.base58_encode(<< 0 >>)
   end
+
+  defp maybe_decode(<< "xpub", _rest::binary >> = encoded_key) do
+    decoded_key = encoded_key
+                  |> Bip32Mnemonic.parse_extended_key
+
+    decoded_key.key
+  end
+  defp maybe_decode(key), do: key
 end
--- lib/block_keys/ethereum/address.ex
+++ lib/block_keys/ethereum/address.ex
@@ -1,21 +1,25 @@
 defmodule BlockKeys.Ethereum.Address do
   alias BlockKeys.Bip32Mnemonic
 
-  def from_public_key(<< "xpub", _rest::binary >> = encoded_key) do
+  def from_public_key(public_key) do
+    public_key
+    |> maybe_decode()
+    |> keccak256()
+    |> to_address
+  end
+
+  defp maybe_decode(<< "xpub", _rest::binary >> = encoded_key) do
     decoded_key = encoded_key
                   |> Bip32Mnemonic.parse_extended_key
 
-    from_public_key(decoded_key.key)
+    decoded_key.key
   end
+  defp maybe_decode(key), do: key
 
-  def from_public_key(public_key) do
-    address_bytes = public_key
-                    |> keccak256()
-    <<_::binary-12, address::binary-20>> = address_bytes
-
+  defp to_address(<<_::binary-12, address::binary-20>>) do
     "0x"
     |> Kernel.<>(address |> Base.encode16(case: :lower))
   end
 
-  def keccak256(data), do: :keccakf1600.hash(:sha3_256, data)
+  defp keccak256(data), do: :keccakf1600.hash(:sha3_256, data)
 end
