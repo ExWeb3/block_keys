f7543bb19ef33d1ae4b792aae1f6ee0a73008e7e
Razvan <tzumby@gmail.com>
2019-01-23 15:07:12 -0500

    Fix main module derive methods

--- lib/block_keys.ex
+++ lib/block_keys.ex
@@ -3,6 +3,7 @@ defmodule BlockKeys do
 
   @private_version_number <<4, 136, 173, 228>>
   @public_version_number  <<4, 136, 178, 30>>
+  @mersenne_prime 2_147_483_647
 
   def derive(extended_key, << "m/", path::binary >>) do
     decoded_key = Bip32Mnemonic.parse_extended_key(extended_key)
@@ -16,26 +17,39 @@ defmodule BlockKeys do
     end
   end
 
-  def derive(extended_key, << "M/", path::binary >>) do
-    decoded_key = Bip32Mnemonic.parse_extended_key(extended_key)
-    
-    xpub = case decoded_key.version_number do
-      @private_version_number ->
-        Bip32Mnemonic.master_public_key(extended_key)
-      @public_version_number ->
-        extended_key
-    end
-
+  def derive(<< "xpub", _rest::binary >> = extended_key, << "M/", path::binary >>) do
     path
     |> String.split("/")
-    |> _derive(xpub)
+    |> _derive(extended_key)
+  end
+
+  def derive(<< "xprv", _rest::binary >> = extended_key, << "M/", path::binary >>) do
+    child_prv = 
+      path
+      |> String.split("/")
+      |> _derive(extended_key)
+
+    Bip32Mnemonic.master_public_key(child_prv)
   end
 
   def _derive([], extended_key), do: extended_key
   def _derive([index | rest], extended_key) do
+    index = case Regex.scan(~r/'/, index) do
+      [] -> 
+        index |> String.to_integer
+      _ ->
+        hardened_index = 
+          index 
+          |> String.replace(~r/'/, "")
+          |> String.to_integer
+          |> Kernel.+(1)
+        hardened_index + @mersenne_prime
+    end
 
-    with child_key = Bip32Mnemonic.child_key(extended_key, index |> String.to_integer) do
+    with child_key = Bip32Mnemonic.child_key(extended_key, index) do
       _derive(rest, child_key)
+    else
+      err -> err
     end
   end
 end
