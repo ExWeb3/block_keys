16b1f9c04f825cabe2d2b583cecbd4793796dbb7
Razvan <tzumby@gmail.com>
2019-02-19 16:28:53 -0500

    Adds more tests for Bip32Mnemonic: child_key_public/2 child_key_private_2 and parse_extended_key/1

--- test/block_keys/bip32_mnemonic_test.exs
+++ test/block_keys/bip32_mnemonic_test.exs
@@ -65,7 +65,6 @@ defmodule Bip32MnemonicTest do
 
   describe "master_public_key/1" do
     test "it returns an extended public key given a Base58check extended private key" do
-      
       mnemonic = "safe result wire cattle sauce luggage couple legend pause rather employ pear trigger live daring unlock music lyrics smoke mistake endorse kite obey siren"
 
       seed = Bip32Mnemonic.generate_seed(mnemonic)
@@ -76,6 +75,91 @@ defmodule Bip32MnemonicTest do
       master_public_key = Bip32Mnemonic.master_public_key(master_private_key)
 
       assert master_public_key == "xpub661MyMwAqRbcGn7BJH7K5WkNcWE9HbpoaKcBJ7agvraQmcb4sXZ57feCnEvDKV37gSV9baYsKvUuRYyD4RKrXt7ciDyKAhLQTbmq5ocYXWZ"
+    end
+  end
+
+  describe "child_key_public/2" do
+    test "it returns the base58 encoded child extended public key given parent extended key and index" do
+      mnemonic = "safe result wire cattle sauce luggage couple legend pause rather employ pear trigger live daring unlock music lyrics smoke mistake endorse kite obey siren"
+
+      seed = Bip32Mnemonic.generate_seed(mnemonic)
+
+      master_private_key = Bip32Mnemonic.master_keys(seed)
+                           |> Bip32Mnemonic.master_private_key
+
+      master_public_key = Bip32Mnemonic.master_public_key(master_private_key)
+                          |> Bip32Mnemonic.parse_extended_key
+
+
+      child_extended_public_key = Bip32Mnemonic.child_key_public(master_public_key, 0)
+
+      assert child_extended_public_key == "xpub686qTu6J49gBYxxSMhXKe9v7h8eBf7uKdDNVrfa5nBhHS7SNqi4kcCwsX8VvirP3yp8eiw3X8a6v1MAzBNTzeiuhVgTgptPLi4C9whoB5Qg"
+    end
+  end
+
+  describe "child_key_private/2" do
+    test "it returns the base58 encoded child extended public key given parent extended key and index" do
+      mnemonic = "safe result wire cattle sauce luggage couple legend pause rather employ pear trigger live daring unlock music lyrics smoke mistake endorse kite obey siren"
+
+      seed = Bip32Mnemonic.generate_seed(mnemonic)
+
+      master_private_key = Bip32Mnemonic.master_keys(seed)
+                           |> Bip32Mnemonic.master_private_key
+                           |> Bip32Mnemonic.parse_extended_key
+
+      child_extended_private_key = Bip32Mnemonic.child_key_private(master_private_key, 0)
+
+      assert child_extended_private_key == "xprv9u7V4PZQDn7tLUsyFfzKH1yP96ohFfBUFzSu4HAUDrAJZK7EJAkW4QdPftAnA6t3SauQvxBmMMbYb8YXYgFuyxEVaQ5tZYD74zkfZu4AuZF"
+    end
+  end
+
+  describe "parse_extended_key/1" do
+    test "it returns a map with all decoded bytes given a base58check private encoded extended key" do
+      mnemonic = "safe result wire cattle sauce luggage couple legend pause rather employ pear trigger live daring unlock music lyrics smoke mistake endorse kite obey siren"
+
+      seed = Bip32Mnemonic.generate_seed(mnemonic)
+
+      master_private_key = Bip32Mnemonic.master_keys(seed)
+                           |> Bip32Mnemonic.master_private_key
+                           |> Bip32Mnemonic.parse_extended_key
+
+      assert master_private_key ==  %{
+              chain_code: <<224, 143, 204, 84, 66, 158, 71, 172, 85, 254, 189,
+                77, 201, 237, 204, 200, 141, 41, 46, 180, 10, 163, 118, 90, 243,
+                218, 113, 120, 161, 74, 161, 20>>,
+              depth: <<0>>,
+              fingerprint: <<0, 0, 0, 0>>,
+              index: <<0, 0, 0, 0>>,
+              key: <<0, 48, 166, 181, 156, 204, 201, 36, 252, 159, 253, 74, 176,
+                140, 92, 1, 240, 214, 164, 4, 103, 151, 187, 37, 93, 137, 25,
+                235, 62, 149, 192, 136, 113>>,
+              version_number: <<4, 136, 173, 228>>
+      }
+    end
+
+    test "it returns a map with all decoded bytes given a base58check public encoded extended key" do
+      mnemonic = "safe result wire cattle sauce luggage couple legend pause rather employ pear trigger live daring unlock music lyrics smoke mistake endorse kite obey siren"
+
+      seed = Bip32Mnemonic.generate_seed(mnemonic)
+
+      master_private_key = Bip32Mnemonic.master_keys(seed)
+                           |> Bip32Mnemonic.master_private_key
+
+      master_public_key = Bip32Mnemonic.master_public_key(master_private_key)
+                          |> Bip32Mnemonic.parse_extended_key
+
+      assert master_public_key ==  %{
+              chain_code: <<224, 143, 204, 84, 66, 158, 71, 172, 85, 254, 189,
+                77, 201, 237, 204, 200, 141, 41, 46, 180, 10, 163, 118, 90, 243,
+                218, 113, 120, 161, 74, 161, 20>>,
+              depth: <<0>>,
+              fingerprint: <<0, 0, 0, 0>>,
+              index: <<0, 0, 0, 0>>,
+              key: <<3, 128, 40, 86, 6, 23, 133, 88, 180, 217, 74, 146, 146, 5,
+                72, 251, 163, 93, 11, 149, 248, 168, 127, 44, 43, 86, 72, 90,
+                205, 149, 136, 223, 33>>,
+              version_number: <<4, 136, 178, 30>>
+      }
     end
   end
 end
