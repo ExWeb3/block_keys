96a98942d3c61465291d95034754fc27b8b22749
Razvan <tzumby@gmail.com>
2019-03-02 00:46:55 -0500

    Updates Bitcoin module with minor refactor

--- lib/block_keys/bitcoin/address.ex
+++ lib/block_keys/bitcoin/address.ex
@@ -9,14 +9,14 @@ defmodule BlockKeys.Bitcoin.Address do
     xpub
     |> maybe_decode()
     |> Crypto.hash160
-    |> Encoding.base58_encode(<< 0 >>)
+    |> Encoding.base58_encode(<<0>>)
   end
 
   defp maybe_decode(<< "xpub", _rest::binary >> = encoded_key) do
-    decoded_key = encoded_key
-                  |> Encoding.decode_extended_key
-
-    decoded_key.key
+    encoded_key
+    |> Encoding.decode_extended_key
+    |> Map.fetch!(:key)
   end
+
   defp maybe_decode(key), do: key
 end
