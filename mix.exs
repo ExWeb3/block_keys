3fe8ef1913ca60449a8a0726e26e72fc22e14ade
Raz <tzumby@gmail.com>
2020-08-22 13:26:09 -0400

    Prepare 0.1.7 release (#25)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BlockKeys.MixProject do
   def project do
     [
       app: :block_keys,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.7",
       description: description(),
       start_permanent: Mix.env() == :prod,
