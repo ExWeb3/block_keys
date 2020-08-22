5a98065d6ac6b30b4afd5bc28884c2e975fe5215
Razvan Draghici <tzumby@gmail.com>
2020-08-22 10:08:08 -0400

    Adds tests for 12 and 3 word mnemonics

--- test/block_keys/mnemonic_test.exs
+++ test/block_keys/mnemonic_test.exs
@@ -4,17 +4,42 @@ defmodule MnemonicTest do
   alias BlockKeys.Mnemonic
 
   describe "phrase_from_entropy" do
-    test "it generates entropy given a 24 word mnemonic with correct checksum" do
+    test "it generates a phrase given 32 bytes of entropy" do
       expected_phrase =
         "safe result wire cattle sauce luggage couple legend pause rather employ pear trigger live daring unlock music lyrics smoke mistake endorse kite obey siren"
 
-      entropy =
-        <<190, 22, 251, 240, 146, 43, 249, 9, 140, 75, 252, 161, 118, 73, 35, 209, 14, 137, 5, 77,
-          231, 112, 145, 240, 175, 51, 70, 244, 156, 246, 101, 254>>
+      entropy_hex = "be16fbf0922bf9098c4bfca1764923d10e89054de77091f0af3346f49cf665fe"
+      entropy_bytes = Base.decode16!(entropy_hex, case: :lower)
 
-      phrase = Mnemonic.generate_phrase(entropy)
+      phrase = Mnemonic.generate_phrase(entropy_bytes)
 
       assert expected_phrase == phrase
+      assert entropy_hex == Mnemonic.entropy_from_phrase(phrase)
+    end
+
+    test "it generates a phrase given 16 bytes of entropy" do
+      expected_phrase =
+        "they air shoot swim divide brief castle little fever size original fiscal"
+
+      entropy_hex = "e080af1a6df3fe37c8e41455793e72ab"
+      entropy_bytes = Base.decode16!(entropy_hex, case: :lower)
+
+      phrase = Mnemonic.generate_phrase(entropy_bytes)
+
+      assert expected_phrase == phrase
+      assert entropy_hex == Mnemonic.entropy_from_phrase(phrase)
+    end
+
+    test "it generates a phrase given 4 bytes of entropy" do
+      expected_phrase = "elite edit very"
+
+      entropy_hex = "4808cfcb"
+      entropy_bytes = Base.decode16!(entropy_hex, case: :lower)
+
+      phrase = Mnemonic.generate_phrase(entropy_bytes)
+
+      assert expected_phrase == phrase
+      assert entropy_hex == Mnemonic.entropy_from_phrase(phrase)
     end
   end
 
@@ -23,9 +48,22 @@ defmodule MnemonicTest do
       mnemonic =
         "safe result wire cattle sauce luggage couple legend pause rather employ pear trigger live daring unlock music lyrics smoke mistake endorse kite obey siren"
 
-      entropy = Mnemonic.entropy_from_phrase(mnemonic)
+      assert Mnemonic.entropy_from_phrase(mnemonic) ==
+               "be16fbf0922bf9098c4bfca1764923d10e89054de77091f0af3346f49cf665fe"
+    end
 
-      assert byte_size(entropy) == 32
+    test "it generates entropy given a 12 word mnemonic with correct checksum" do
+      mnemonic = "assault tank plastic awful speed tool little today glance chief notice dance"
+
+      assert Mnemonic.entropy_from_phrase(mnemonic) ==
+               "0d9bb698085d11c960a71a62a4fa5b9b"
+    end
+
+    test "it generates entropy given a 3 word mnemonic" do
+      mnemonic = "lawsuit orbit nut"
+
+      assert Mnemonic.entropy_from_phrase(mnemonic) ==
+               "7e337e5e"
     end
 
     test "it returns an error if checksum is not correct" do
@@ -45,6 +83,24 @@ defmodule MnemonicTest do
 
       assert seed ==
                "e8006d573be37f252c41d00dcd98a25abbd8ae3a1bdf500922faa6b29777b8a706997cb246587028687fe1fcc001da461f8c0eaa12d04219c1b1b9ad2fc808f1"
+    end
+
+    test "it generates seed given a 12 word mnemonic with correct checksum" do
+      mnemonic = "demise elevator first honey olympic flee ankle sure cube chaos kiss tray"
+
+      seed = Mnemonic.generate_seed(mnemonic)
+
+      assert seed ==
+               "bf99a6cb6658e0e5a483eca7fab81a2090031dd49d199e71975d2d3422df5496e8f1b48382d2fbc6a9bb8128bbcbbab4d092445fc5152aa2be08744ac6dc31cc"
+    end
+
+    test "it generates seed given a 3 word mnemonic" do
+      mnemonic = "grab naive furnace"
+
+      seed = Mnemonic.generate_seed(mnemonic)
+
+      assert seed ==
+               "a55601e89ec1296be238d8525b7dcc1e0a9a7740d365c9b8592264aa87b6f6335388f42cac49ebbd743a93adcd43c2d7fb7eb43de1963a9e325cbd938ba737f1"
     end
 
     test "it returns an error given a 24 word mnemonic with incorrect checksum" do
